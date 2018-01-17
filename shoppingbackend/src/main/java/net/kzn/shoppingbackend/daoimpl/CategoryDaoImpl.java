eaefbd2ea93229437c1d02f25b29e065db02fe77
onimarufl <moonoimooham@gmail.com>
2018-01-17 23:20:57 +0700

    V7 Success

--- shoppingbackend/src/main/java/net/kzn/shoppingbackend/daoimpl/CategoryDaoImpl.java
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/daoimpl/CategoryDaoImpl.java
@@ -1,9 +1,9 @@
 package net.kzn.shoppingbackend.daoimpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,74 +12,67 @@ import net.kzn.shoppingbackend.dao.CategoryDAO;
 import net.kzn.shoppingbackend.dto.Category;
 
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDaoImpl implements CategoryDAO {
-	
+
 	@Autowired
 	private SessionFactory sessionFactory;
-	
-	
-	private static List<Category> categories = new ArrayList<>();
 
-	static {
-		
-		Category category = new Category();
-		
-		//add category
-		category.setId(1);
-		category.setName("Television");
-		category.setDescription("This is some TV.....");
-		category.setImageURL("CAT_1.png");
-		categories.add(category);
-		
-		category = new Category();
-		
-		//add category 2
-		category.setId(2);
-		category.setName("Mobile");
-		category.setDescription("This is some Mobile.....");
-		category.setImageURL("CAT_2.png");
-		categories.add(category);
-		
-	category = new Category();
-		
-		//add category 3
-		category.setId(3);
-		category.setName("Notebook");
-		category.setDescription("This is some Notebook.....");
-		category.setImageURL("CAT_3.png");
-		categories.add(category);
-		
-	}
-	
 	@Override
-
-	
 	public List<Category> list() {
-		// TODO Auto-generated method stub
-		return categories;
+
+		String selectActiveCategory = "FROM Category WHERE active = :active";
+		
+		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
+		
+		query.setParameter("active", true);
+		
+		
+		return query.getResultList();
 	}
 
 	@Override
 	public Category get(int id) {
-		
-		for(Category category : categories) {
-			if(category.getId() == id) return category;
-		}
-		return null;
+
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
 	}
 
 	@Override
-	@Transactional
+
 	public boolean add(Category category) {
 
 		try {
 			sessionFactory.getCurrentSession().persist(category);
 			return true;
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
+			return false;
 		}
-		return false;
+
+	}
+
+	@Override
+	public boolean update(Category category) {
+		try {
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(Category category) {
+		
+		category.setActive(false);
+		try {
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
 	}
 
 }
--- shoppingbackend/src/test/java/net/kzn/shoppingbackend/test/CategoryTestCase.java
+++ shoppingbackend/src/test/java/net/kzn/shoppingbackend/test/CategoryTestCase.java
@@ -26,7 +26,7 @@ public class CategoryTestCase {
 		categoryDAO = (CategoryDAO)context.getBean("categoryDAO");
 	}
 	
-	@Test
+/*	@Test
 	public void testAddCategory() {
 		
 		category = new Category();
@@ -37,5 +37,78 @@ public class CategoryTestCase {
 		
 		assertEquals("Successfully",true,categoryDAO.add(category));
 	
+	}*/
+	
+	/*@Test
+	public void testGetCategory() {
+		
+		category = categoryDAO.get(3);
+		
+		assertEquals("Successfully fetched a single","Television",category.getName());
+	}*/
+	
+	/*@Test
+	public void testUpdateCategory() {
+		
+		category = categoryDAO.get(3);
+		
+		category.setName("TV");
+		
+		assertEquals("Successfully update a single",true,categoryDAO.update(category));
 	}
+	*/
+	
+	/*@Test
+	public void testDeleteCategory() {
+		
+		category = categoryDAO.get(3);
+		
+		
+		assertEquals("Successfully delete a single",true,categoryDAO.delete(category));
+	}
+	*/
+	
+	/*@Test
+	public void testListCategory() {
+		
+		
+		assertEquals("Successfully fetched a category",3,categoryDAO.list().size());
+	}*/
+	
+	@Test
+	public void testCRUDCategory() {
+		
+		category = new Category();
+		
+		category.setName("Notebook");
+		category.setDescription("This is some Notebook.....");
+		category.setImageURL("CAT_1.png");
+		
+		assertEquals("Successfully",true,categoryDAO.add(category));
+		
+		category = new Category();
+		category.setName("Television");
+		category.setDescription("This is some TV.....");
+		category.setImageURL("CAT_2.png");
+		
+		assertEquals("Successfully",true,categoryDAO.add(category));
+		
+		
+		category = categoryDAO.get(2);
+		
+		category.setName("TV");
+		
+		assertEquals("Successfully update a single",true,categoryDAO.update(category));
+	
+		
+		//Delete
+		assertEquals("Successfully delete a single",true,categoryDAO.delete(category));
+		
+		
+		//fetching
+		assertEquals("Successfully fetched a category",1,categoryDAO.list().size());
+
+	}
+	
+	
 }
